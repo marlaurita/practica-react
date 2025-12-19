aa27996fca8a6eeef03293c090e33caaba45edd4
Maria Laura Jaramillo (Thoughtworks) <mariajaramillo.thoughtworks@latam.com>
2025-12-18 21:23:26 -0300

    chore: useState

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Card } from "./components/index";
 import hospImg from "./assets/images/hospedaje.jpg";
+import hospDefault from "./assets/images/image-default.png";
 
 function App() {
   const [count, setCount] = useState(0);
-  const hosts = [
+  const [hosts, setHosts] = useState(null);
+  //const [isLoading, setIsLoading] = useState(true);
+
+  const hostsService = [
     {
       id: 1,
       name: "cabaña en Quatro Barras, Brasil",
@@ -69,24 +73,30 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={() => setHosts(hostsService)}>
+          Llenar Hospedajes
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">Hola soy Maria Laura</p>
-      <div className="container">
-        {hosts.map((host) => (
-          <Card
-            texto={host.name}
-            key={host.id}
-            isSuperHost={host.isSuperHost}
-            imageUrl={host.imageUrl}
-            handleClick={() => {
-              saludoHospedaje(host.name);
-            }}
-          />
-        ))}
-      </div>
+      {hosts != null && (
+        <div className="container">
+          {hosts.map((host) => (
+            <Card
+              texto={host.name}
+              key={host.id}
+              isSuperHost={host.isSuperHost}
+              imageUrl={host.imageUrl}
+              handleClick={() => {
+                saludoHospedaje(host.name);
+              }}
+            />
+          ))}
+        </div>
+      )}
+      {hosts == null && <img src={hospDefault}></img>}
     </>
   );
 }
